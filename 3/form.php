4b9f47c37129c7ccf4f0ddd9c029c498924f8f38
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-02-23 19:09:34 +0300

    Update form.php

--- 3/form.php
+++ 3/form.php
@@ -1,5 +1,5 @@
 <div id="form"><h2>Форма</h2></div>
-<form action="URL_отправки_данных" method="POST">
+<form action="process_form.php" method="POST">
     ФИО:<br>
     <input name="fio" type="text" placeholder="ФИО"><br>
     
@@ -13,13 +13,27 @@
     <input name="bdate" type="date"><br>
 
     Пол:<br>
-    <label><input class="radio" name="radio" checked="checked" type="radio" value="female">Женский</label>
-    <label><input class="radio" name="radio" type="radio" value="male">Мужской</label><br> 
+    <label><input class="radio" name="radio" checked="checked" type="radio" value="Female">Женский</label>
+    <label><input class="radio" name="radio" type="radio" value="Male">Мужской</label><br> 
     
     Любимый язык программирования:
     <br>
     <select id="abilities" name="abilities[]" multiple="multiple">
       <?php 
+      $abilities = array(
+        '1' => 'Pascal',
+        '2' => 'C',
+        '3' => 'C++',
+        '4' => 'JavaScript',
+        '5' => 'PHP',
+        '6' => 'Python',
+        '7' => 'Java',
+        '8' => 'Haskell',
+        '9' => 'Clojure',
+        '10' => 'Prolog',
+        '11' => 'Scala',
+        '12' => 'Go'
+      );
       foreach ($abilities as $key => $value) {
         printf('<option value="%s">%s</option>', $key, $value);
       } 
