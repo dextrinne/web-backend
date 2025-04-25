85c92d89c71c33ea939c2990df46400820bea4d7
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-04-26 01:59:23 +0300

    Update user.php

--- skillswap/user.php
+++ skillswap/user.php
@@ -25,7 +25,6 @@ $user_skills = get_user_skills($conn, $user_id);
 <head>
     <meta charset="UTF-8">
     <title>Профиль пользователя</title>
-    <link rel="stylesheet" href="./style/style.css">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
