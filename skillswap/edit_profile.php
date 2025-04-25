22802f6ff1fa27829df58d3b171e512bd68cea4c
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-04-26 01:31:34 +0300

    Update edit_profile.php

--- skillswap/edit_profile.php
+++ skillswap/edit_profile.php
@@ -194,13 +194,16 @@ $user = get_user_data($conn, $user_id);
 
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <title>Редактировать профиль</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
+
 <body>
     <!-- Шапка -->
     <nav class="mainNav">
@@ -213,44 +216,59 @@ $user = get_user_data($conn, $user_id);
     </nav>
 
     <div class="container">
-        <h2>Редактировать профиль</h2>
-
-        <?php if ($user): ?>
-            <form class="form-horizontal" action="actions/update_profile.php" method="post">
-                <div class="form-group">
-                    <label>Имя:</label>
-                    <input type="text" class="form-control" name="first_name" value="<?php echo htmlspecialchars($user['first_name']); ?>" required>
-                </div>
-                <div class="form-group">
-                    <label>Фамилия:</label>
-                    <input type="text" class="form-control" name="last_name" value="<?php echo htmlspecialchars($user['last_name']); ?>" required>
-                </div>
-                <div class="form-group">
-                    <label>Почта:</label>
-                    <input type="email" class="form-control" name="email" value="<?php echo htmlspecialchars($user['email']); ?>" required>
-                </div>
-                <div class="form-group">
-                    <label>Дата рождения:</label>
-                    <input type="date" class="form-control" name="birthdate" value="<?php echo htmlspecialchars($user['birthdate']); ?>">
-                </div>
-                <div class="form-group">
-                    <label>Пол:</label>
-                    <div class="radio-group">
-                        <label>
-                            <input class="radio" name="gender" type="radio" value="Female" <?php if($user['gender'] == 'Female') echo 'checked'; ?> />Женский
-                        </label>
-                        <label>
-                            <input class="radio" name="gender" type="radio" value="Male" <?php if($user['gender'] == 'Male') echo 'checked'; ?> />Мужской
-                        </label>
+        <div class="row">
+            <div class="col-md-offset-3 col-md-6">
+                <div class="tab" role="tabpanel">
+                    <div class="tab-content tabs">
+                        <div role="tabpanel" class="tab-pane fade in active" id="Section1">
+                            <?php if ($user): ?>
+                                <form class="form-horizontal" action="actions/update_profile.php" method="post">
+                                    <div class="form-group">
+                                        <label>Имя:</label>
+                                        <input type="text" class="form-control" name="first_name"
+                                            value="<?php echo htmlspecialchars($user['first_name']); ?>" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Фамилия:</label>
+                                        <input type="text" class="form-control" name="last_name"
+                                            value="<?php echo htmlspecialchars($user['last_name']); ?>" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Почта:</label>
+                                        <input type="email" class="form-control" name="email"
+                                            value="<?php echo htmlspecialchars($user['email']); ?>" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Дата рождения:</label>
+                                        <input type="date" class="form-control" name="birthdate"
+                                            value="<?php echo htmlspecialchars($user['birthdate']); ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Пол:</label>
+                                        <div class="radio-group">
+                                            <label>
+                                                <input class="radio" name="gender" type="radio" value="Female" <?php if ($user['gender'] == 'Female')
+                                                    echo 'checked'; ?> />Женский
+                                            </label>
+                                            <label>
+                                                <input class="radio" name="gender" type="radio" value="Male" <?php if ($user['gender'] == 'Male')
+                                                    echo 'checked'; ?> />Мужской
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-default">Сохранить изменения</button>
+                                    </div>
+                                </form>
+                            <?php else: ?>
+                                <p>Ошибка: Не удалось получить информацию о пользователе.</p>
+                            <?php endif; ?>
+                        </div>
                     </div>
                 </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-default">Сохранить изменения</button>
-                </div>
-            </form>
-        <?php else: ?>
-            <p>Ошибка: Не удалось получить информацию о пользователе.</p>
-        <?php endif; ?>
+            </div>
+        </div>
     </div>
 </body>
+
 </html>
