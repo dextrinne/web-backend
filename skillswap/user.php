29c7755aaf0d25117f5bb7ef9fd29c0868331e98
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-04-26 12:59:25 +0300

    Update user.php

--- skillswap/user.php
+++ skillswap/user.php
@@ -29,12 +29,11 @@ $other_users_skills = get_other_users_skills($conn, $user_id);
 <head>
     <meta charset="UTF-8">
     <title>Профиль пользователя</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
    
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <style>
