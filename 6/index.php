ab76baa2b387af71137dd8df33c53dc6be833f05
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-05-03 22:46:27 +0300

    Update index.php

--- 6/index.php
+++ 6/index.php
@@ -1,34 +1,9 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 echo "<link rel='stylesheet' href='style.css'>";
-
-// Соединение с базой данных.
-$user = 'u68595';
-$pass = '6788124';
-$db = new PDO(
-    'mysql:host=localhost;dbname=u68595',
-    $user,
-    $pass,
-    [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-);
-
-// Функция для получения списка языков программирования.
-function getAbilities($db)
-{
-    try {
-        $abilities = [];
-        $data = $db->query("SELECT id, name FROM language")->fetchAll();
-        foreach ($data as $ability) {
-            $name = $ability['name'];
-            $lang_id = $ability['id'];
-            $abilities[$lang_id] = $name;
-        }
-        return $abilities;
-    } catch (PDOException $e) {
-        print('Error: ' . $e->getMessage());
-        exit();
-    }
-}
+include('./actions/db.php');
+include('./actions/functions.php');
+include('./actions/validation.php');
 
 $abilities = getAbilities($db);
 
@@ -36,33 +11,10 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-// Функция для получения данных пользователя из базы данных
-function getUserData($db, $login)
-{
-    try {
-        $stmt = $db->prepare("
-            SELECT 
-                u.fio, 
-                u.tel, 
-                u.email, 
-                u.bdate, 
-                u.gender, 
-                u.bio, 
-                u.ccheck,
-                GROUP_CONCAT(ul.lang_id) as abilities
-            FROM user u
-            INNER JOIN user_login ulg ON u.id = ulg.user_id
-            LEFT JOIN user_language ul ON u.id = ul.user_id
-            WHERE ulg.login = ?
-            GROUP BY u.id
-        ");
-        $stmt->execute([$login]);
-        $user_data = $stmt->fetch(PDO::FETCH_ASSOC);
-        return $user_data;
-    } catch (PDOException $e) {
-        print('Error: ' . $e->getMessage());
-        exit();
-    }
+$abilities = getAbilities($db);
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
@@ -286,17 +238,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         setcookie('radio_error', '', 100000);
         setcookie('bio_error', '', 100000);
         setcookie('ccheck_error', '', 100000);
-    }
-
-    // Генерируем логин и пароль
-    function generateRandomString($length = 10) {
-        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
-        }
-        return $randomString;
     }
 
     $login = generateRandomString(8);
