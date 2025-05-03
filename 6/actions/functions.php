93c0cdd5073aae7e9f3526842348d2cc47df3ea2
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-05-03 22:01:16 +0300

    Update functions.php

--- 6/actions/functions.php
+++ 6/actions/functions.php
@@ -81,3 +81,14 @@ function getLanguageStats($db) {
         die('Ошибка получения статистики: ' . $e->getMessage());
     }
 }
+
+function generateRandomString($length = 10) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+?>
