665f8a2a6f05e99222a98b9f36fba5bba59a4054
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-05-03 21:41:17 +0300

    Update validation.php

--- 6/actions/validation.php
+++ 6/actions/validation.php
@@ -1,7 +1,7 @@
 <?php
 function validateFormData($db, &$errors, &$values, $abilities) {
-    $errors = false;
-    
+    $errors = FALSE;
+
     if (empty($_POST['fio'])) {
         setcookie('fio_error', '1', time() + 24 * 60 * 60);
         $errors = TRUE;
@@ -64,94 +64,15 @@ function validateFormData($db, &$errors, &$values, $abilities) {
         $errors = TRUE;
     }
 
+    setcookie('fio_value', $_POST['fio'], time() + 30 * 24 * 60 * 60);
+    setcookie('tel_value', $_POST['tel'], time() + 30 * 24 * 60 * 60);
+    setcookie('email_value', $_POST['email'], time() + 30 * 24 * 60 * 60);
+    setcookie('abilities_value', !empty($fav_languages) ? implode(',', $fav_languages) : '', time() + 30 * 24 * 60 * 60);
+    setcookie('bdate_value', $_POST['bdate'], time() + 30 * 24 * 60 * 60);
+    setcookie('radio_value', $_POST['radio'], time() + 30 * 24 * 60 * 60);
+    setcookie('bio_value', $_POST['bio'], time() + 30 * 24 * 60 * 60);
+    setcookie('ccheck_value', $_POST['ccheck'], time() + 30 * 24 * 60 * 60);
+
     return !$errors;
-}
-
-function handleValidationErrors(&$messages) {
-    $errorsMap = [
-        'fio' => [
-            '1' => 'Введите ФИО.',
-            '2' => 'ФИО не должно превышать 150 символов.',
-            '3' => 'ФИО должно содержать только буквы и пробелы.'
-        ],
-        'tel' => ['1' => 'Введите корректный номер телефона.'],
-        'email' => ['1' => 'Введите корректный email.'],
-        'abilities' => ['1' => 'Выберите любимый язык программирования.'],
-        'bdate' => ['1' => 'Введите корректную дату рождения.'],
-        'radio' => ['1' => 'Выберите пол.'],
-        'bio' => [
-            '1' => 'Заполните биографию.',
-            '2' => 'Количество символов в поле "биография" не должно превышать 512.',
-            '3' => 'Поле "биография" содержит недопустимые символы.'
-        ],
-        'ccheck' => ['1' => 'Подтвердите ознакомление с контрактом.']
-    ];
-
-    foreach ($errorsMap as $field => $errorTypes) {
-        if (!empty($_COOKIE["{$field}_error"])) {
-            $errorCode = $_COOKIE["{$field}_error"];
-            if (isset($errorTypes[$errorCode])) {
-                $messages[] = '<div class="error">' . $errorTypes[$errorCode] . '</div>';
-            }
-            setcookie("{$field}_error", '', time() - 3600);
-        }
-    }
-}
-
-function fillUserValues($db, $login = null) {
-    $values = [];
-    
-    if ($login) {
-        if ($userData = getUserData($db, $login)) {
-            foreach (['fio', 'tel', 'email', 'bdate', 'bio', 'ccheck'] as $field) {
-                $values[$field] = htmlspecialchars($userData[$field] ?? '');
-            }
-            $values['radio'] = htmlspecialchars($userData['gender'] ?? '');
-            $values['abilities'] = !empty($userData['abilities']) ? explode(',', $userData['abilities']) : [];
-        }
-    } else {
-        foreach (['fio', 'tel', 'email', 'bdate', 'radio', 'bio', 'ccheck'] as $field) {
-            $values[$field] = $_COOKIE["{$field}_value"] ?? '';
-        }
-        $values['abilities'] = !empty($_COOKIE['abilities_value']) ? explode(',', $_COOKIE['abilities_value']) : [];
-    }
-    
-    return $values;
-}
-
-function updateUserData($db, $user_id, $post_data, $abilities = []) {
-    try {
-        $db->beginTransaction();
-        
-        // Обновляем основные данные пользователя
-        $stmt = $db->prepare("
-            UPDATE user SET 
-                fio = ?, tel = ?, email = ?, bdate = ?, 
-                gender = ?, bio = ?, ccheck = ?
-            WHERE id = ?
-        ");
-        $stmt->execute([
-            $post_data['fio'], $post_data['tel'], $post_data['email'], $post_data['bdate'],
-            $post_data['gender'], $post_data['bio'], isset($post_data['ccheck']) ? 1 : 0,
-            $user_id
-        ]);
-        
-        // Обновляем языки программирования
-        $stmt = $db->prepare("DELETE FROM user_language WHERE user_id = ?");
-        $stmt->execute([$user_id]);
-        
-        if (!empty($abilities)) {
-            $stmt = $db->prepare("INSERT INTO user_language (user_id, lang_id) VALUES (?, ?)");
-            foreach ($abilities as $lang_id) {
-                $stmt->execute([$user_id, $lang_id]);
-            }
-        }
-        
-        $db->commit();
-        return true;
-    } catch (PDOException $e) {
-        $db->rollBack();
-        return false;
-    }
 }
 ?>
