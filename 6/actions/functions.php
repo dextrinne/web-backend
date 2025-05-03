1595ab38686f345d479e278f7aefecab489f1d31
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-05-03 21:47:59 +0300

    Update functions.php

--- 6/actions/functions.php
+++ 6/actions/functions.php
@@ -81,3 +81,67 @@ function getLanguageStats($db) {
         die('Ошибка получения статистики: ' . $e->getMessage());
     }
 }
+
+function updateUserData($db, $user_id, $post_data, $abilities) {
+    try {
+        $stmt = $db->prepare("
+            UPDATE user 
+            SET fio = ?, tel = ?, email = ?, bdate = ?, gender = ?, bio = ?, ccheck = ?
+            WHERE id = ?
+        ");
+        $stmt->execute([
+            $post_data['fio'],
+            $post_data['tel'],
+            $post_data['email'],
+            $post_data['bdate'],
+            $post_data['radio'],
+            $post_data['bio'],
+            isset($post_data['ccheck']) ? 1 : 0,
+            $user_id
+        ]);
+        
+        $db->prepare("DELETE FROM user_language WHERE user_id = ?")->execute([$user_id]);
+        
+        if (!empty($abilities)) {
+            $stmt = $db->prepare("INSERT INTO user_language (user_id, lang_id) VALUES (?, ?)");
+            foreach ($abilities as $ability) {
+                $stmt->execute([$user_id, $ability]);
+            }
+        }
+        
+        return true;
+    } catch (PDOException $e) {
+        error_log('Error updating user data: ' . $e->getMessage());
+        return false;
+    }
+}
+
+function fillUserValues($db, $login = null) {
+    if ($login) {
+        $user_data = getUserData($db, $login);
+        if ($user_data) {
+            return [
+                'fio' => $user_data['fio'] ?? '',
+                'tel' => $user_data['tel'] ?? '',
+                'email' => $user_data['email'] ?? '',
+                'bdate' => $user_data['bdate'] ?? '',
+                'radio' => $user_data['gender'] ?? '',
+                'bio' => $user_data['bio'] ?? '',
+                'ccheck' => $user_data['ccheck'] ?? '',
+                'abilities' => isset($user_data['abilities']) ? explode(',', $user_data['abilities']) : []
+            ];
+        }
+    }
+    
+    return [
+        'fio' => $_COOKIE['fio_value'] ?? '',
+        'tel' => $_COOKIE['tel_value'] ?? '',
+        'email' => $_COOKIE['email_value'] ?? '',
+        'bdate' => $_COOKIE['bdate_value'] ?? '',
+        'radio' => $_COOKIE['radio_value'] ?? '',
+        'bio' => $_COOKIE['bio_value'] ?? '',
+        'ccheck' => $_COOKIE['ccheck_value'] ?? '',
+        'abilities' => isset($_COOKIE['abilities_value']) ? explode(',', $_COOKIE['abilities_value']) : []
+    ];
+}
+?>
