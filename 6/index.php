5753b72a49cfeddcec5d941b70a97667cbc04008
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-05-03 22:54:34 +0300

    Update index.php

--- 6/index.php
+++ 6/index.php
@@ -11,12 +11,6 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-$abilities = getAbilities($db);
-
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $messages = array();
     $values = array();  // Инициализация массива $values
@@ -46,7 +40,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $logoutButton = '';
     }
 
-
     // Сообщение об успешном сохранении.
     if (!empty($_COOKIE['save'])) {
         setcookie('save', '', 100000);
@@ -65,7 +58,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         }
     }
 
-
     // Складываем признак ошибок в массив.
     $errors = array();
     $errors['fio'] = !empty($_COOKIE['fio_error']);
@@ -76,7 +68,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $errors['radio'] = !empty($_COOKIE['radio_error']);
     $errors['bio'] = !empty($_COOKIE['bio_error']);
     $errors['ccheck'] = !empty($_COOKIE['ccheck_error']);
-
 
     // Выдаем сообщения об ошибках.
     if ($errors['fio']) {
@@ -149,83 +140,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $values['ccheck'] = empty($_COOKIE['ccheck_value']) ? '' : $_COOKIE['ccheck_value'];
 
     echo $logoutButton; 
-
     include('form.php');
-} else {  
-    $errors = FALSE;
-
-    if (empty($_POST['fio'])) {
-        setcookie('fio_error', '1', time() + 24 * 60 * 60);
-        $errors = TRUE;
-    } else {
-        if (strlen($_POST['fio']) > 150) {
-            setcookie('fio_error', '2', time() + 24 * 60 * 60);
-            $errors = TRUE;
-        } elseif (!preg_match("/^[a-zA-Zа-яА-ЯёЁ\s]+$/u", $_POST['fio'])) {
-            setcookie('fio_error', '3', time() + 24 * 60 * 60);
-            $errors = TRUE;
-        }
-    }
-
-    if (empty($_POST['tel']) || !preg_match('/^\+7\d{10}$/', $_POST['tel'])) {
-        setcookie('tel_error', '1', time() + 24 * 60 * 60);
-        $errors = TRUE;
-    }
-    if (empty($_POST["email"]) || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-        setcookie('email_error', '1', time() + 24 * 60 * 60);
-        $errors = TRUE;
-    }
-
-    $fav_languages = $_POST["abilities"] ?? []; // Получаем массив из формы
-    if (empty($fav_languages)) {
-        setcookie('abilities_error', '1', time() + 24 * 60 * 60);
-        $errors = TRUE;
-    } else {
-        foreach ($fav_languages as $ability) {
-            if (empty($abilities[$ability])) {
-                setcookie('abilities_error', '1', time() + 24 * 60 * 60);
-                $errors = TRUE;
-            }
-        }
-    }
-
-    if (empty($_POST['bdate']) || !preg_match('/^\d{4}-\d{2}-\d{2}$/', $_POST['bdate'])) {
-        setcookie('bdate_error', '1', time() + 24 * 60 * 60);
-        $errors = TRUE;
-    }
-    if (empty($_POST['radio'])) {
-        setcookie('radio_error', '1', time() + 24 * 60 * 60);
-        $errors = TRUE;
-    }
-
-    if (empty($_POST['bio'])) {
-        setcookie('bio_error', '1', time() + 24 * 60 * 60);
-        $errors = TRUE;
-    } elseif(strlen($_POST['bio']) > 512){
-        setcookie('bio_error', '2', time() + 24 * 60 * 60);
-        $errors = TRUE;
-    } elseif(preg_match('/[<>{}\[\]]|<script|<\?php/i', $_POST['bio'])){
-        setcookie('bio_error', '3', time() + 24 * 60 * 60);
-        $errors = TRUE;
-    }
-
-    if (empty($_POST['ccheck'])) {
-        setcookie('ccheck_error', '1', time() + 24 * 60 * 60);
-        $errors = TRUE;
-    }
-
-    // Сохраняем ранее введенное в форму значение на месяц.
+} else {
+    // Валидация формы с использованием функции из validation.php
+    $validationResult = validateFormData($db, $errors, $values, $abilities);
+    
+    // Сохраняем введенные значения в куки
     setcookie('fio_value', $_POST['fio'], time() + 30 * 24 * 60 * 60);
     setcookie('tel_value', $_POST['tel'], time() + 30 * 24 * 60 * 60);
     setcookie('email_value', $_POST['email'], time() + 30 * 24 * 60 * 60);
-    setcookie('abilities_value', !empty($fav_languages) ? implode(',', $fav_languages) : '', time() + 30 * 24 * 60 * 60);
+    setcookie('abilities_value', !empty($_POST['abilities']) ? implode(',', $_POST['abilities']) : '', time() + 30 * 24 * 60 * 60);
     setcookie('bdate_value', $_POST['bdate'], time() + 30 * 24 * 60 * 60);
     setcookie('radio_value', $_POST['radio'], time() + 30 * 24 * 60 * 60);
     setcookie('bio_value', $_POST['bio'], time() + 30 * 24 * 60 * 60);
     setcookie('ccheck_value', $_POST['ccheck'], time() + 30 * 24 * 60 * 60);
 
-    if ($errors) {
-        // При наличии ошибок перезагружаем страницу и завершаем работу скрипта.
+    if (!$validationResult) {
         header('Location: index.php');
         exit();
     } else {
@@ -244,78 +174,75 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $pass = generateRandomString(20);
 
     try {
-      if (isset($_SESSION['login'])) {
-        // Получаем user_id
-        $stmt = $db->prepare("SELECT user_id FROM user_login WHERE login = ?");
-        $stmt->execute([$_SESSION['login']]);
-        $user_data = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (isset($_SESSION['login'])) {
+            // Получаем user_id
+            $stmt = $db->prepare("SELECT user_id FROM user_login WHERE login = ?");
+            $stmt->execute([$_SESSION['login']]);
+            $user_data = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        if ($user_data && isset($user_data['user_id'])) {
-          $user_id = $user_data['user_id'];
+            if ($user_data && isset($user_data['user_id'])) {
+                $user_id = $user_data['user_id'];
 
-          // Обновляем данные пользователя
-          $stmt = $db->prepare("
-                  UPDATE user 
-                  SET fio = ?, 
-                      tel = ?, 
-                      email = ?, 
-                      bdate = ?, 
-                      gender = ?, 
-                      bio = ?, 
-                      ccheck = ?
-                  WHERE id = ?
-              ");
-          $stmt->execute([
-              $_POST['fio'],
-              $_POST['tel'],
-              $_POST['email'],
-              $_POST['bdate'],
-              $_POST['radio'],
-              $_POST['bio'],
-              isset($_POST["ccheck"]) ? 1 : 0,
-              $user_id
-          ]);
+                // Обновляем данные пользователя
+                $stmt = $db->prepare("
+                        UPDATE user 
+                        SET fio = ?, 
+                            tel = ?, 
+                            email = ?, 
+                            bdate = ?, 
+                            gender = ?, 
+                            bio = ?, 
+                            ccheck = ?
+                        WHERE id = ?
+                    ");
+                $stmt->execute([
+                    $_POST['fio'],
+                    $_POST['tel'],
+                    $_POST['email'],
+                    $_POST['bdate'],
+                    $_POST['radio'],
+                    $_POST['bio'],
+                    isset($_POST["ccheck"]) ? 1 : 0,
+                    $user_id
+                ]);
 
-          // Удаляем старые значения языков программирования
-          $stmt = $db->prepare("DELETE FROM user_language WHERE user_id = ?");
-          $stmt->execute([$user_id]);
+                // Удаляем старые значения языков программирования
+                $stmt = $db->prepare("DELETE FROM user_language WHERE user_id = ?");
+                $stmt->execute([$user_id]);
 
-          // Добавляем новые значения языков программирования
-          if (isset($_POST['abilities']) && is_array($_POST['abilities'])) {
-              $stmt = $db->prepare("INSERT INTO user_language (user_id, lang_id) VALUES (?, ?)");
-              foreach ($_POST['abilities'] as $ability) {
-                  $stmt->execute([$user_id, $ability]);
-              }
-          }
-        }
-      }
-      else {
-        $stmt = $db->prepare("INSERT INTO user (fio, tel, email, bdate, gender, bio, ccheck) VALUES (?, ?, ?, ?, ?, ?, ?)");
-        $stmt->execute([$_POST['fio'], $_POST['tel'], $_POST['email'], $_POST['bdate'], $_POST['radio'], $_POST['bio'], isset($_POST["ccheck"]) ? 1 : 0]);
-
-        $user_id = $db->lastInsertId();
-
-        $stmt = $db->prepare("INSERT INTO user_login (user_id, login, password) VALUES (?, ?, ?)");
-        $stmt->execute([$user_id, $login, password_hash($pass, PASSWORD_DEFAULT)]);
-
-        if (isset($_POST['abilities']) && is_array($_POST['abilities'])) {
-            $stmt = $db->prepare("INSERT INTO user_language (user_id, lang_id) VALUES (?, ?)");
-            foreach ($_POST['abilities'] as $ability) {
-                $stmt->execute([$user_id, $ability]);
+                // Добавляем новые значения языков программирования
+                if (isset($_POST['abilities']) && is_array($_POST['abilities'])) {
+                    $stmt = $db->prepare("INSERT INTO user_language (user_id, lang_id) VALUES (?, ?)");
+                    foreach ($_POST['abilities'] as $ability) {
+                        $stmt->execute([$user_id, $ability]);
+                    }
+                }
             }
+        } else {
+            $stmt = $db->prepare("INSERT INTO user (fio, tel, email, bdate, gender, bio, ccheck) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            $stmt->execute([$_POST['fio'], $_POST['tel'], $_POST['email'], $_POST['bdate'], $_POST['radio'], $_POST['bio'], isset($_POST["ccheck"]) ? 1 : 0]);
+
+            $user_id = $db->lastInsertId();
+
+            $stmt = $db->prepare("INSERT INTO user_login (user_id, login, password) VALUES (?, ?, ?)");
+            $stmt->execute([$user_id, $login, password_hash($pass, PASSWORD_DEFAULT)]);
+
+            if (isset($_POST['abilities']) && is_array($_POST['abilities'])) {
+                $stmt = $db->prepare("INSERT INTO user_language (user_id, lang_id) VALUES (?, ?)");
+                foreach ($_POST['abilities'] as $ability) {
+                    $stmt->execute([$user_id, $ability]);
+                }
+            }
+
+            setcookie('login', $login, time() + 3600);
+            setcookie('pass', $pass, time() + 3600);
         }
-
-        setcookie('login', $login, time() + 3600);
-        setcookie('pass', $pass, time() + 3600);
-      }
-
     } catch (PDOException $e) {
         print('Error in DB: ' . $e->getMessage());
         exit();
     }
 
     setcookie('save', '1', time() + 3600);  
-
     header('Location: index.php');
     exit();
 }
