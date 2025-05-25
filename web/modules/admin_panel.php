10fbc8e1aafa46a41c728c2ef909619136665a1f
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-05-25 14:53:31 +0300

    1

--- web/modules/admin_panel.php
+++ web/modules/admin_panel.php
@@ -1,6 +1,5 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-require_once 'includes/security_headers.php';
 session_start();
 include('./actions/db.php');
 include('./actions/functions.php');
