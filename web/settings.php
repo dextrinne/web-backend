228b2b8d0b079046b74639aa6df26c40e4f20693
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-05-25 05:16:42 +0300

    1

--- web/settings.php
+++ web/settings.php
@@ -42,7 +42,10 @@ $conf = array(
 
 $urlconf = array(
   '' => array('module' => 'front'),
-  '/^admin$/' => ['module' => 'admin_panel', 'auth' => 'auth_basic'],
+  '/^admin$/' => array(
+    'module' => 'admin_panel', 
+    'auth' => 'auth_basic'
+  ),
   '/^admin\/(\d+)$/' => array('module' => 'admin_panel', 'auth' => 'auth_basic'),
   '/^edit_user\/(\d+)$/' => array('module' => 'edit_user', 'auth' => 'auth_basic'),
   '/^form$/' => array('module' => 'form'),
