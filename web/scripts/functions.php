47ae94193204b1ea06c34e201f8c144f5d3999e0
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-05-25 19:16:17 +0300

    1

--- web/scripts/functions.php
+++ web/scripts/functions.php
@@ -3,18 +3,19 @@
  * @param string $filename 
  * @param array $data 
  */
+if (!function_exists('include_frontend_file')) {
+    function include_frontend_file(string $filename, array $data = []): void
+    {
+        global $conf;
 
-function include_frontend_file(string $filename, array $data = []): void
-{
-    global $conf;
+        $filepath = $_SERVER['DOCUMENT_ROOT'] . $conf['frontend_dir'] . '/' . $filename;
 
-    $filepath = $_SERVER['DOCUMENT_ROOT'] . $conf['frontend_dir'] . '/' . $filename;
-
-    if (file_exists($filepath)) {
-        extract($data);
-        include $filepath;
-    } else {
-        echo "<!-- Файл frontend не найден: " . htmlspecialchars($filename) . " -->";
+        if (file_exists($filepath)) {
+            extract($data);
+            include $filepath;
+        } else {
+            echo "<!-- Файл frontend не найден: " . htmlspecialchars($filename) . " -->";
+        }
     }
 }
 
@@ -22,11 +23,10 @@ function include_frontend_file(string $filename, array $data = []): void
  * @param string $path 
  * @return string 
  */
-function frontend_url(string $path): string
-{
-    global $conf;
-    return $conf['basedir'] . 'frontend/' . $path;
+if (!function_exists('frontend_url')) {
+    function frontend_url(string $path): string
+    {
+        global $conf;
+        return $conf['basedir'] . 'frontend/' . $path;
+    }
 }
-
-
-
