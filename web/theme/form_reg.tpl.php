ae020cf9af2fdfaea1478fc85c084d9f268287de
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-05-25 04:34:52 +0300

    я простила

--- web/theme/form_reg.tpl.php
+++ web/theme/form_reg.tpl.php
@@ -25,7 +25,7 @@
                 <a href="./login.php" class="btn btn-secondary">Войти</a>
             <?php endif; ?>
             
-            <a href="./modules/admin_panel.php" class="btn btn-admin" target="_blank" 
+            <a href="./modules/admin_panel.tpl.php" class="btn btn-admin" target="_blank" 
             style="background-color: #dc3545; color: white; border: none;"> Администратор</a>
         </div>
 
