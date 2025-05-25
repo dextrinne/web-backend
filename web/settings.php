9dc196c098c6d5f27bc5dfd1b0361f5a023eda7c
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-05-25 04:46:33 +0300

    моя попытка номер пять

--- web/settings.php
+++ web/settings.php
@@ -43,7 +43,7 @@ $conf = array(
 $urlconf = array(
   '' => array('module' => 'front'),
   '/^admin$/' => array('module' => 'admin_panel', 'auth' => 'auth_basic'),
-  '/^admin\/(\d+)$/' => array('module' => 'admin_panel', 'auth' => 'auth_basic'),
+  '/^web-backend\/web\/admin$/' => array('module' => 'admin_panel', 'auth' => 'auth_basic'),
   '/^edit_user\/(\d+)$/' => array('module' => 'edit_user', 'auth' => 'auth_basic'),
   '/^form$/' => array('module' => 'form'),
   '/^success$/' => array('module' => 'success'),
