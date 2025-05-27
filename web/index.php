cf5c65f0bf230281641eedaf85b69593120f2699
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-05-27 16:12:20 +0300

    1

--- web/index.php
+++ web/index.php
@@ -19,7 +19,7 @@ $request = array(
 
 $response = init($request, $urlconf, $db);
 
-if (!empty($response['headers'])) {
+/*if (!empty($response['headers'])) {
     foreach ($response['headers'] as $key => $value) {
         if (is_string($key)) {
             header(sprintf('%s: %s', $key, $value));
@@ -28,7 +28,7 @@ if (!empty($response['headers'])) {
         }
     }
 }
-/*if (!empty($response['entity'])) {
+if (!empty($response['entity'])) {
     print ($response['entity']);
 }*/
 
