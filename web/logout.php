241f2dba62b268f06f345855f07ea8792cc2d14f
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-06-01 01:12:09 +0300

    1

--- web/logout.php
+++ web/logout.php
@@ -8,7 +8,7 @@ $return_to_admin = isset($_GET['return_to_admin']);
 session_destroy();
 
 if ($return_to_admin) {
-    header("Location: /web-backend/web/admin_panel.php");
+    header("Location: /web-backend/web/modules/admin_panel.php");
 } else {
     header("Location: /web-backend/web/#form-anchor");
 }
